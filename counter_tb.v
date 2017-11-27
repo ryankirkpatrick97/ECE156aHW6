e78d433bfe2897407a4ea3030465037233b465a2
Ryan C. Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2017-11-26 22:22:06 -0800

    Added inital tb for counter

---
+++ counter_tb.v
@@ -0,0 +1,19 @@
+
+module counter_tb();
+
+reg clk, reset;
+wire [3:0] count;
+
+    counter_B cb(clk, reset, count);
+
+always #5 clk = ~clk;
+
+initial begin
+    clk = 0;
+    reset = 1;
+
+    #7.5
+    reset = 0;
+
+end
+endmodule
