d9f352b846de7a4b44ce5786cc56523bfe6105e6
Ryan C. Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2017-11-27 15:07:54 -0800

    Changed counter implementation

--- counter_B.v
+++ counter_B.v
@@ -1,9 +1,13 @@
 
 module counter_B(input clk, reset, output reg [3:0] count);
 
+    reg logic1, logic2;
+
     always @(posedge clk, posedge reset) begin
-        if(reset==1 | count == 10) count = 0;
-        else if (count <= 10) begin
+        logic1 = reset | (count == 10);
+        logic2 = (count <= 10);
+        if(logic1) count = 0;
+        else if (logic2) begin
             count = count + 1;
         end//counting
     end//always
