8cd4e5e16b20b1f7bb0faab29641aa69d04ee62b
Ryan C. Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2017-11-27 15:16:39 -0800

    Added different version of counter

---
+++ counter_S.v
@@ -0,0 +1,25 @@
+
+module counter_S(clk, reset, count);
+    input clk, reset;
+    output reg [3:0] count;
+
+    always @ (posedge clk)
+    begin
+        if (reset)
+        begin
+            count <= 4'h0;
+        end
+        else
+        begin
+            if (count < 4'ha)
+            begin
+                count <= count + 1;
+            end
+            else
+            begin
+                count <= 4'h0;
+            end
+        end
+    end
+
+endmodule
