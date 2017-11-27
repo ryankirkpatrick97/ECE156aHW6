04d9ddcd315951989309c092a7efb9ce476fd598
Ryan Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2017-11-27 15:33:23 -0800

    compiled counter_S and modified counter_tb to be a miter

--- counter_S.v
+++ counter_S.v
@@ -1,18 +1,25 @@
 
-module counter_S(clk, reset, count);
-    input clk, reset;
-    output reg [3:0] count;
+module counter_S ( clk, reset, count );
+  output [3:0] count;
+  input clk, reset;
+  wire   N9, N10, N11, N12, n3, n5, n6, n11, n12, n13, n14, n15, n16, n17, n18,
+         n19;
 
-    always @ (posedge clk, posedge reset) begin
-        if (reset)begin
-            count <= 0;
-        end else begin
-            if (count < 10) begin
-                count <= count + 1;
-            end else begin
-                count <= 0;
-            end
-        end
-    end
-
+  FD2 \count_reg[0]  ( .D(N9), .CP(clk), .CD(n3), .Q(count[0]), .QN(n18) );
+  FD2 \count_reg[1]  ( .D(N10), .CP(clk), .CD(n3), .Q(count[1]), .QN(n6) );
+  FD2 \count_reg[2]  ( .D(N11), .CP(clk), .CD(n3), .Q(count[2]), .QN(n5) );
+  FD2 \count_reg[3]  ( .D(N12), .CP(clk), .CD(n3), .Q(count[3]), .QN(n19) );
+  IV U13 ( .A(reset), .Z(n3) );
+  AO4 U14 ( .A(n19), .B(n11), .C(n12), .D(n13), .Z(N12) );
+  OR2 U15 ( .A(n5), .B(n6), .Z(n13) );
+  MUX21L U16 ( .A(n14), .B(n15), .S(n5), .Z(N11) );
+  OR2 U17 ( .A(n12), .B(n6), .Z(n15) );
+  AO6 U18 ( .A(n6), .B(n16), .C(N9), .Z(n14) );
+  IV U19 ( .A(n17), .Z(N9) );
+  MUX21L U20 ( .A(n17), .B(n12), .S(n6), .Z(N10) );
+  OR2 U21 ( .A(n11), .B(n18), .Z(n12) );
+  ND2 U22 ( .A(n18), .B(n16), .Z(n17) );
+  IV U23 ( .A(n11), .Z(n16) );
+  AO6 U24 ( .A(n5), .B(n6), .C(n19), .Z(n11) );
 endmodule
+
--- counter_tb.v
+++ counter_tb.v
@@ -2,9 +2,14 @@
 module counter_tb();
 
 reg clk, reset;
-wire [3:0] count;
+wire [3:0] countB, countS, countM;
+wire miterOut;
+    counter_B cb(clk, reset, countB);
+    counter_S cs(clk, reset, countS);
 
-    counter_B cb(clk, reset, count);
+    assign countM = countB ^ countS;
+    or(miterOut, countM[3], countM[2], countM[1], countM[0]);
+
 
 always #5 clk = ~clk;
 
