35b023990eea3cb894ccac4918c7b58e0c3e6979
Ryan C. Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2017-11-27 00:29:13 -0800

    Made logicals into wires

--- counter_B.v
+++ counter_B.v
@@ -1,11 +1,12 @@
 
 module counter_B(input clk, reset, output reg [3:0] count);
 
-    wire first_logical, second_logical;
-    assign first_logical = reset | (count == 10);
-    assign second_logical = (count <= 10);
+    reg first_logical, second_logical;
 
     always @(posedge clk, posedge reset) begin
+        first_logical = reset | (count == 10);
+        second_logical = (count <= 10);
+
         if(first_logical) count <= 0;
         else if (second_logical) begin
             count <= count + 1;
