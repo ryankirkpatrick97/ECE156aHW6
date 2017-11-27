6e1be8aa6f5fbe0e57447da859891bd3bbf8e30d
Ryan C. Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2017-11-27 00:22:14 -0800

    Changed counter to be blocking

--- counter_B.v
+++ counter_B.v
@@ -4,7 +4,7 @@ module counter_B(input clk, reset, output reg [3:0] count);
     always @(posedge clk, posedge reset) begin
         if(reset | count == 10) count <= 0;
         else if (count <= 10) begin
-            count = count + 1;
+            count <= count + 1;
         end//counting
     end//always
 
