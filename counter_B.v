82e9b93042f6e99a2287ee9f675001a0f77e452a
Ryan C. Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2017-11-27 00:27:29 -0800

    Changed coutner to have simple identifier in if

--- counter_B.v
+++ counter_B.v
@@ -1,9 +1,13 @@
 
 module counter_B(input clk, reset, output reg [3:0] count);
 
+    wire first_logical, second_logical;
+    assign first_logical = reset | (count == 10);
+    assign second_logical = (count <= 10);
+
     always @(posedge clk, posedge reset) begin
-        if(reset==1 | count == 10) count <= 0;
-        else if (count <= 10) begin
+        if(first_logical) count <= 0;
+        else if (second_logical) begin
             count <= count + 1;
         end//counting
     end//always
