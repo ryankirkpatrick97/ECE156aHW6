c736aef26d3449f3b3f8ed81b6d7d7ed31d504a6
Ryan C. Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2017-11-27 00:24:02 -0800

    Change reset condition in if statement

--- counter_B.v
+++ counter_B.v
@@ -2,7 +2,7 @@
 module counter_B(input clk, reset, output reg [3:0] count);
 
     always @(posedge clk, posedge reset) begin
-        if(reset | count == 10) count <= 0;
+        if(reset==1 | count == 10) count <= 0;
         else if (count <= 10) begin
             count <= count + 1;
         end//counting
